5d648620020651138c900eb1409bbfed37ddecea
Alec032 <133861676+Alec032@users.noreply.github.com>
2024-05-29 02:12:22 -0400

    Add files via upload

---
+++ TxtFileCompressor-main/build/CMakeFiles/FileCompressor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileCompressor.dir/compress.c.o"
+  "CMakeFiles/FileCompressor.dir/compress.c.o.d"
+  "FileCompressor"
+  "FileCompressor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FileCompressor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TxtFileCompressor-main/build/CMakeFiles/FileCompressor.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for FileCompressor.
+# This may be replaced when dependencies are built.
